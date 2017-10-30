bf49a6a3d41592d0724f900d29018f4edb0532ff
Stephanie Yung <shies425@newschool.edu>
2017-10-30 03:14:17 +0000

    week 09 assignment

---
+++ week09/week09.js
@@ -0,0 +1,43 @@
+var request = require('request');
+const { Client } = require('pg');
+
+// PARTICLE PHOTON
+var device_id = process.env.PHOTON_ID;
+var access_token = process.env.PHOTON_TOKEN;
+var particle_variable = 'jsonString';
+var device_url = 'https://api.particle.io/v1/devices/' + device_id + '/' + particle_variable + '?access_token=' + access_token;
+
+// AWS RDS POSTGRESQL INSTANCE
+var db_credentials = new Object();
+db_credentials.user = 'shies425';
+db_credentials.host = process.env.AWSRDS_EP;
+db_credentials.database = 'fridge_phototemp';
+db_credentials.password = process.env.AWSRDS_PW;
+db_credentials.port = 5432;
+
+var getAndWriteData = function() {
+    // Make request to the Particle API to get sensor values
+    request(device_url, function(error, response, body) {
+        // Store sensor values in variables
+        var device_json_string = JSON.parse(body).result;
+        var photo = JSON.parse(device_json_string).photovalue;
+        var tempC = JSON.parse(device_json_string).tempvalue;
+
+        // Connect to the AWS RDS Postgres database
+        const client = new Client(db_credentials);
+        client.connect();
+
+        // Construct a SQL statement to insert sensor values into a table
+        var thisQuery = "INSERT INTO fridge_readings VALUES (" + photo + "," + tempC + ", DEFAULT);";
+        console.log(thisQuery); // for debugging
+
+        // Connect to the AWS RDS Postgres database and insert a new row of sensor values
+        client.query(thisQuery, (err, res) => {
+            console.log(err, res);
+            client.end();
+        });
+    });
+};
+
+// write a new row of sensor data every second
+setInterval(getAndWriteData, 1000);
