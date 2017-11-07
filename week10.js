f61386cea58f0256c828571eedd9270b97319eee
Stephanie Yung <shies425@newschool.edu>
2017-11-07 14:01:34 +0000

    updated var q code

--- week10.js
+++ week10.js
@@ -15,20 +15,19 @@ app.get('/', function(req, res) {
     const client = new Pool(db_credentials);
 
     // SQL query
-    var q = `SELECT EXTRACT(DAY FROM fridge_readings AT TIME ZONE 'America/New_York') as sensorday, 
-             EXTRACT(MONTH FROM fridge_readings AT TIME ZONE 'America/New_York') as sensormonth, 
-             count(*) as num_obs, 
-             max(lightsensor) as max_light, 
-             min(lightsensor) as min_light,
-             max(tempsensor) as max_temp, 
-             min(tempsensor) as min_temp
-             FROM fridge_readings 
-             GROUP BY sensormonth, sensorday;`;
+    var q = `SELECT fridgetime AT TIME ZONE 'EST' AS fridgetime
+            photo, temperature
+            FROM fridge_readings
+            WHERE photo > 100
+            ORDER BY fridgetime;`;
              
     client.connect();
     client.query(q, (qerr, qres) => {
-        res.send(qres.rows);
-        console.log('responded to request');
+        if (qerr) console.log (qerr);
+        else {
+            res.send(qres.rows);
+            console.log('responded to request');
+        }
     });
     client.end();
 });
